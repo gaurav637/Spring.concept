04db8fc1764bb27aa3fe05fed3f20e57c4f192de
GauravNegi <negigaurav637@gmail.com>
2024-01-11 00:50:10 +0530

    add new class

--- src/main/java/Collection_Types/Student.java
+++ src/main/java/Collection_Types/Student.java
@@ -1,6 +1,14 @@
 package Collection_Types;
-
+import java.util.List;
+import java.util.Set;
+import java.util.Map;
 public class Student {
+
+    private String name;
+    private List<String> phone;
+    private Set<String> address;
+    private Map<String,String> course;
+
 
 
 }
