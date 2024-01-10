a73f4d207f1436d51ed62683af266de0cf2cf07b
GauravNegi <negigaurav637@gmail.com>
2024-01-11 01:26:55 +0530

    add collection type

--- src/main/java/Collection_Types/MainClass.java
+++ src/main/java/Collection_Types/MainClass.java
@@ -1,7 +1,16 @@
 package Collection_Types;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class MainClass {
     public static void main(String args[]){
         System.out.println("hello world in main class");
+
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+        Student objStudent = (Student) context.getBean("student1");
+        System.out.println(objStudent);
+
+
     }
 }
--- src/main/java/Collection_Types/Student.java
+++ src/main/java/Collection_Types/Student.java
@@ -1,4 +1,5 @@
 package Collection_Types;
+
 import java.util.List;
 import java.util.Set;
 import java.util.Map;
@@ -10,5 +11,52 @@ public class Student {
     private Map<String,String> course;
 
 
+    public String getName() {
+        return name;
+    }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getPhone() {
+        return phone;
+    }
+
+    public void setPhone(List<String> phone) {
+        this.phone = phone;
+    }
+
+    public Set<String> getAddress() {
+        return address;
+    }
+
+    public void setAddress(Set<String> address) {
+        this.address = address;
+    }
+
+    public Map<String, String> getCourse() {
+        return course;
+    }
+
+    public void setCourse(Map<String, String> course) {
+        this.course = course;
+    }
+
+    public Student(String name,List phone,Set address,Map course){
+        super();
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+        this.course = course;
+    }
+
+    public Student(){
+        super();
+    }
+
+    public String toString(){
+        return " Student [ student name= " + name + " student phone = "
+                + phone + " student address "  + address + " course = " + course + "]";
+    }
 }
