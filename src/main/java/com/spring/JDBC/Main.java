2f86ebac629dad011a022a99a150d58754a91782
GauravNegi <negigaurav637@gmail.com>
2024-01-14 23:39:52 +0530

    insert data

--- src/main/java/com/spring/JDBC/Main.java
+++ src/main/java/com/spring/JDBC/Main.java
@@ -11,13 +11,16 @@ public class Main {
 		System.out.println("heloo world welcome to jdbc");
 		
 		
-		ApplicationContext con = new ClassPathXmlApplicationContext("/Spring.concept/src/main/java/com/spring/JDBC/Spring.xml");
+		ApplicationContext con = new ClassPathXmlApplicationContext("Spring.concept/src/main/java/com/spring/JDBC/Spring.xml");
 		
 		JdbcTemplate jt = con.getBean("jdbcTemplate1" ,JdbcTemplate.class);
 		
 		System.out.println("jdbc effected row -> "+jt);
 		
+		String sql = "INSERT INTO student(id,name,city,rollNo)VALUES(?,?,?)";
 		
+		int n = jt.update(sql,102,"rahul","jalandhar",10);
+		System.out.println("data is insert in table "+n);
 	}
 
 }
