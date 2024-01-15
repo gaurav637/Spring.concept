ff79d72ce39d2504bb0f7518e1a81d8acc4e1e7c
GauravNegi <negigaurav637@gmail.com>
2024-01-15 21:31:14 +0530

    add function

--- src/main/java/com/spring/JDBC/Main.java
+++ src/main/java/com/spring/JDBC/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		System.out.println("heloo world welcome to jdbc");
 		
 		
-		ApplicationContext con = new ClassPathXmlApplicationContext("Spring.concept/src/main/java/com/spring/JDBC/Spring.xml");
+		ApplicationContext con = new ClassPathXmlApplicationContext("com/spring/JDBC/SpringJDBC.xml");
 		
 		JdbcTemplate jt = con.getBean("jdbcTemplate1" ,JdbcTemplate.class);
 		
